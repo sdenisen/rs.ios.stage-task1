53817dd8789afcb289f7e84b38101df44ffde5f3
sdenisenko <sdenisen@ymail.com>
2021-05-07 12:51:05 +0300

    feat: implement isValidNumber method; test passed;

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,16 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+    if ([self length] == 0)
+        return false;
+    
+    for (int i=0; i<[self length]; i++){
+        unichar a = [self characterAtIndex:i];
+        if (a<=57 && a>=48)
+            continue;
+        return false;
+    }
+    return true;
 }
 
 @end
