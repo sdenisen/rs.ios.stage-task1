a13c0204ea1b37bbecdfe50cead8206d36f2380f
sdenisenko <sdenisen@ymail.com>
2021-05-08 01:20:41 +0300

    feat: implement leader board ranks; test passed;

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -10,7 +10,37 @@
 
 - (NSArray <NSNumber *> *)trackLeaderBoard:(NSArray <NSNumber *> *)rankedArray
                                playerArray:(NSArray <NSNumber *> *)playerArray {
-    return @[];
+    
+    NSOrderedSet *mySet = [[NSOrderedSet alloc] initWithArray:rankedArray];
+    NSMutableArray *a = [[NSMutableArray alloc] initWithArray:[mySet array]];
+    
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    
+    int k = 0;
+    for (int i=0; i<[playerArray count]; i++){
+        int prev_k = k;
+        for(int j=0; j<[a count]; j++){
+            if([a[j] intValue]<[playerArray[i] intValue]){
+                [a insertObject:playerArray[i] atIndex:j];
+                [result addObject:@(j+1)];
+                k++;
+                break;
+            }
+            if([a[j] intValue]==[playerArray[i] intValue]){
+                [result addObject:@(j+1)];
+                k++;
+                break;
+            }
+        }
+        if (prev_k == k){
+            [a addObject:playerArray[i]];
+            [result addObject:@([a count])];
+            k++;
+        }
+    }
+//    NSArray <NSNumber *> *rr = [[NSArray alloc] initWithArray:result];
+    
+    return [result copy];
 }
 
 @end
