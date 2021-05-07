2fcfd8e5918d0d200f0930e2e7ac345c1e0a20d8
sdenisenko <sdenisen@ymail.com>
2021-05-07 20:36:42 +0300

    feat: implement number converter; tests passed;

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,19 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    if (number == nil)
+        return [[NSArray alloc] init];
+    
+    int tmp = abs([number intValue]);
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    while (tmp!=0){
+        int r = tmp%10;
+        NSString *t = [[NSString alloc] initWithFormat:@"%d", r];
+        [result addObject: t];
+        tmp /= 10;
+    }
+    
+    return [result copy];
 }
 
 @end
